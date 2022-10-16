c7f759313d31b1fcd96ac6941ba0f7a968470899
edualt <eduardosebastianhdz503@gmail.com>
2022-10-16 17:37:20 -0500

    feat(entities): add Product identity

--- src/main/java/com/school/ecommerceupch/entities/Product.java
+++ src/main/java/com/school/ecommerceupch/entities/Product.java
@@ -1,2 +1,35 @@
-package com.school.ecommerceupch.entities;public class Product {
+package com.school.ecommerceupch.entities;
+
+import com.school.ecommerceupch.entities.pivots.ProductCategory;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Setter @Getter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200)
+    private String title;
+
+    private String description;
+
+    private Integer stock;
+
+    private Float price;
+
+    @OneToMany(mappedBy = "category")
+    private List<ProductCategory> productCategories;
+
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+
 }
