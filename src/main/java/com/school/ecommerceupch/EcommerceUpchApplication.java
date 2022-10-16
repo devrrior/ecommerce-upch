214388aa9ac7042b8bda114fa1f286bf8d904c48
Fernando Guerrero <devrrior@gmail.com>
2022-10-16 11:50:07 -0500

    feat(project): set up project

---
+++ src/main/java/com/school/ecommerceupch/EcommerceUpchApplication.java
@@ -0,0 +1,13 @@
+package com.school.ecommerceupch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUpchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceUpchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/school/ecommerceupch/EcommerceUpchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.ecommerceupch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUpchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
