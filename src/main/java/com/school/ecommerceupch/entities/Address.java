c410985ad8f125a61e77ec226bf224e19fd4906a
rodrick <213480@ids.upchiapas.edu.mx>
2022-10-17 01:11:59 -0500

    add Address entity

--- src/main/java/com/school/ecommerceupch/entities/Address.java
+++ src/main/java/com/school/ecommerceupch/entities/Address.java
@@ -15,16 +15,16 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
-    @Column(unique = true, length = 255)
+    @Column(length = 255)
     private String street;
 
-    @Column(unique = true, length = 50)
+    @Column( length = 50)
     private String zipcode;
 
-    @Column(unique = true, length = 100)
+    @Column(length = 100)
     private String state;
 
-    @Column(unique = true, length = 100)
+    @Column(length = 100)
     private String country;
 
 
