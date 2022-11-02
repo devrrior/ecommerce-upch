0281474d1a4672e0a673841c39a1854c3867efe9
Fernando Guerrero <devrrior@gmail.com>
2022-11-02 11:05:29 -0600

    fix(controllers): update exceptions handlers

--- src/main/java/com/school/ecommerceupch/controllers/advices/ApplicationExceptionHandler.java
+++ src/main/java/com/school/ecommerceupch/controllers/advices/ApplicationExceptionHandler.java
@@ -1,13 +1,9 @@
 package com.school.ecommerceupch.controllers.advices;
 
-import com.school.ecommerceupch.controllers.exceptions.UserAlreadyExistsException;
-import com.school.ecommerceupch.controllers.exceptions.UserNotFoundException;
-import com.school.ecommerceupch.controllers.exceptions.UserRoleAlreadyExistsException;
-import com.school.ecommerceupch.controllers.exceptions.UserRoleNotFoundException;
+import com.school.ecommerceupch.controllers.dtos.responses.BaseResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -16,45 +12,21 @@ import java.util.Map;
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            errorMap.put(error.getField(), error.getDefaultMessage());
-        });
-        return errorMap;
-    }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(UserNotFoundException.class)
-    public Map<String, String> handleBussinesException(UserNotFoundException ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("errorMessage", ex.getMessage());
-        return errorMap;
-    }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<BaseResponse> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> errors = new HashMap<>();
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public Map<String, String> handleExistsException(UserAlreadyExistsException ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("errorMessage", ex.getMessage());
-        return errorMap;
-    }
+        String message = ex.getMessage();
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(UserRoleNotFoundException.class)
-    public Map<String, String> handleBussinesException(UserRoleNotFoundException ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("errorMessage", ex.getMessage());
-        return errorMap;
-    }
+        errors.put("message", message);
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(UserRoleAlreadyExistsException.class)
-    public Map<String, String> handleExistsException(UserRoleAlreadyExistsException ex) {
-        Map<java.lang.String, java.lang.String> errorMap = new HashMap<>();
-        errorMap.put("errorMessage", ex.getMessage());
-        return errorMap;
+        BaseResponse baseResponse = BaseResponse.builder()
+                .data(errors)
+                .message("Operation failed")
+                .success(Boolean.FALSE)
+                .httpStatus(HttpStatus.BAD_REQUEST).build();
+
+        return new ResponseEntity<>(baseResponse, baseResponse.getHttpStatus());
     }
 }
--- src/main/java/com/school/ecommerceupch/controllers/exceptions/UserRoleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.school.ecommerceupch.controllers.exceptions;
-
-public class UserRoleNotFoundException extends Exception{
-    public UserRoleNotFoundException(String message) {
-        super(message);
-    }
-}
