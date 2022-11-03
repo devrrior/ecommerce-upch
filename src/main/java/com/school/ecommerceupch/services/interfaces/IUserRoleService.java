e2db92e5ac3d2f941a693f182731121bca22a5b7
Fernando Guerrero <devrrior@gmail.com>
2022-11-02 23:09:14 -0600

    feat(services/interfaces/IUserRoleService): add getAllRolesByUserId method

--- src/main/java/com/school/ecommerceupch/services/interfaces/IUserRoleService.java
+++ src/main/java/com/school/ecommerceupch/services/interfaces/IUserRoleService.java
@@ -1,10 +1,18 @@
 package com.school.ecommerceupch.services.interfaces;
 
 import com.school.ecommerceupch.controllers.dtos.responses.BaseResponse;
+import com.school.ecommerceupch.entities.Role;
+import com.school.ecommerceupch.entities.pivots.UserRole;
+
+import java.util.List;
 
 public interface IUserRoleService {
 
     BaseResponse listAllUsersByRoleId(Long id);
 
     BaseResponse listAllRolesByUserId(Long id);
+
+    List<Role> getAllRolesByUserId(Long id);
+
+    UserRole create(Long userId, Long roleId);
 }
