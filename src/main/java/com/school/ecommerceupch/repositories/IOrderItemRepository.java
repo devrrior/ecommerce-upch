f54aec649e4d041faf113b3644d6a60fb7ed5b80
rodrick <213480@ids.upchiapas.edu.mx>
2022-11-03 01:09:57 -0600

    "Create OrderItem Service "

--- src/main/java/com/school/ecommerceupch/repositories/IOrderItemRepository.java
+++ src/main/java/com/school/ecommerceupch/repositories/IOrderItemRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.school.ecommerceupch.entities.OrderItem;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IOrderItemRepository extends JpaRepository <OrderItem, Long> {
 
-
+        Optional<OrderItem>findById(Long id);
 }
