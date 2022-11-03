c406fdc230582e200723e7a7dc1b3b3b837f8474
edualt <eduardosebastianhdz503@gmail.com>
2022-11-02 23:04:17 -0600

    fix(FileServiceImpl): change variable name from upload method

--- src/main/java/com/school/ecommerceupch/services/interfaces/IFileService.java
+++ src/main/java/com/school/ecommerceupch/services/interfaces/IFileService.java
@@ -4,7 +4,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface IFileService {
 
-    String upload(MultipartFile multipartFile, Long idUser);
+    String upload(MultipartFile multipartFile, Long idProduct);
 
     void delete(String filename);
 
