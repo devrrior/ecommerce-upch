aaf0ee35dbfc3948bec319ab7d0c57df32abc6ec
Fernando Guerrero <devrrior@gmail.com>
2022-11-02 23:12:13 -0600

    feat(services/UserServiceImpl): add default role when User is created

--- src/main/java/com/school/ecommerceupch/services/UserServiceImpl.java
+++ src/main/java/com/school/ecommerceupch/services/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.school.ecommerceupch.services;
 import com.school.ecommerceupch.controllers.dtos.requests.CreateUserRequest;
 import com.school.ecommerceupch.controllers.dtos.requests.UpdateUserRequest;
 import com.school.ecommerceupch.controllers.dtos.responses.BaseResponse;
-import com.school.ecommerceupch.entities.User;
 import com.school.ecommerceupch.entities.Role;
+import com.school.ecommerceupch.entities.User;
+import com.school.ecommerceupch.entities.pivots.UserRole;
 import com.school.ecommerceupch.repositories.IUserRepository;
 import com.school.ecommerceupch.services.interfaces.IRoleService;
+import com.school.ecommerceupch.services.interfaces.IUserRoleService;
 import com.school.ecommerceupch.services.interfaces.IUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,16 +17,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements IUserService {
     private final IUserRepository repository;
-    private final IRoleService userRoleService;
+    private final IRoleService roleService;
+    private final IUserRoleService userRoleService;
 
-    public UserServiceImpl(IUserRepository repository, IRoleService userRoleService) {
+    public UserServiceImpl(IUserRepository repository, IRoleService roleService, IUserRoleService userRoleService) {
         this.repository = repository;
+        this.roleService = roleService;
         this.userRoleService = userRoleService;
     }
 
     @Override
     public BaseResponse create(CreateUserRequest request) {
+        Role defaultRole = roleService.findOneAndEnsureExistByName("ROLE_USER");
+
         User user = repository.save(from(request));
+
+        user = addRoleToUser(defaultRole, user);
 
         return BaseResponse.builder()
                 .data(user)
@@ -32,6 +40,16 @@ public class UserServiceImpl implements IUserService {
                 .success(Boolean.TRUE)
                 .httpStatus(HttpStatus.CREATED)
                 .build();
+    }
+
+    private User addRoleToUser(Role defaultRole, User user) {
+        UserRole userRole = userRoleService.create(user.getId(), defaultRole.getId());
+
+        user.getUserRoles().add(userRole);
+
+        user = repository.save(user);
+
+        return user;
     }
 
     @Override
@@ -74,7 +92,8 @@ public class UserServiceImpl implements IUserService {
                 .build();
     }
 
-    private User findOneAndEnsureExistById(Long id) {
+    @Override
+    public User findOneAndEnsureExistById(Long id) {
         return repository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
     }
 
@@ -86,8 +105,6 @@ public class UserServiceImpl implements IUserService {
         user.setDateOfBirth(request.getDateOfBirth());
         user.setPassword(new BCryptPasswordEncoder().encode(request.getPassword()));
 
-        Role role = userRoleService.findOneAndEnsureExistByName("USER");
-        user.setRole(role);
         return user;
     }
 
@@ -102,5 +119,6 @@ public class UserServiceImpl implements IUserService {
 
         return user;
     }
+
 
 }
