0330387480123cadaad224e5f1cd38df15c45f9e
Fernando Guerrero <devrrior@gmail.com>
2022-11-02 23:11:44 -0600

    feat(services/UserDetailsServiceImpl): add getAuthorities method

--- src/main/java/com/school/ecommerceupch/services/UserDetailsServiceImpl.java
+++ src/main/java/com/school/ecommerceupch/services/UserDetailsServiceImpl.java
@@ -1,21 +1,39 @@
 package com.school.ecommerceupch.services;
 
+import com.school.ecommerceupch.entities.Role;
 import com.school.ecommerceupch.entities.User;
 import com.school.ecommerceupch.repositories.IUserRepository;
 import com.school.ecommerceupch.security.UserDetailsImpl;
+import com.school.ecommerceupch.services.interfaces.IUserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private IUserRepository repository;
 
+    @Autowired
+    private IUserRoleService userRoleService;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = repository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Email not found"));
-        return new UserDetailsImpl(user);
+        List<Role> roles = userRoleService.getAllRolesByUserId(user.getId());
+
+        return new UserDetailsImpl(user, getAuthorities(roles));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
