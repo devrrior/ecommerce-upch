9e3259ff275882ded54ca35f17c21d6263e6418f
edualt <eduardosebastianhdz503@gmail.com>
2022-11-02 23:01:42 -0600

    fix(Order): add OrderItem relationship

--- src/main/java/com/school/ecommerceupch/entities/Order.java
+++ src/main/java/com/school/ecommerceupch/entities/Order.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -24,4 +25,6 @@ public class Order {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
 }
