31c1e95e8423a7093b5498bee8dcff88c90d9d00
edualt <eduardosebastianhdz503@gmail.com>
2022-11-04 09:19:56 -0600

    feat(CreateCategoryRequest): add CreateCategoryRequest

---
+++ src/main/java/com/school/ecommerceupch/controllers/dtos/requests/CreateCategoryRequest.java
@@ -0,0 +1,18 @@
+package com.school.ecommerceupch.controllers.dtos.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter @Setter
+public class CreateCategoryRequest {
+
+    @NotNull @NotBlank
+    private String name;
+
+    @NotBlank
+    private List<Long> productCategoriesIds;
+}
