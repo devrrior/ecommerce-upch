9cbad1e4aa4e7038fb98b8150507b9f03cdfc62f
Fernando Guerrero <devrrior@gmail.com>
2022-11-03 21:03:17 -0600

    feat(controller/exceptions): create new Exceptions

---
+++ src/main/java/com/school/ecommerceupch/controllers/exceptions/AccessDeniedException.java
@@ -0,0 +1,8 @@
+package com.school.ecommerceupch.controllers.exceptions;
+
+public class AccessDeniedException extends Exception {
+
+    public AccessDeniedException(String msg) {
+        super(msg);
+    }
+}
