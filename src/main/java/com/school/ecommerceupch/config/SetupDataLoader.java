940f603b64199c0dde40026bf09af7d87e4f1a57
Fernando Guerrero <devrrior@gmail.com>
2022-11-04 08:22:07 -0600

    feat(config/SetupDataLoader): add default order status

--- src/main/java/com/school/ecommerceupch/config/SetupDataLoader.java
+++ src/main/java/com/school/ecommerceupch/config/SetupDataLoader.java
@@ -1,8 +1,10 @@
 package com.school.ecommerceupch.config;
 
+import com.school.ecommerceupch.entities.OrderStatus;
 import com.school.ecommerceupch.entities.Role;
 import com.school.ecommerceupch.entities.User;
 import com.school.ecommerceupch.entities.pivots.UserRole;
+import com.school.ecommerceupch.repositories.IOrderStatusRepository;
 import com.school.ecommerceupch.repositories.IRoleRepository;
 import com.school.ecommerceupch.repositories.IUserRepository;
 import com.school.ecommerceupch.services.RoleServiceImpl;
@@ -31,18 +33,17 @@ public class SetupDataLoader implements
 
     private IUserRoleService userRoleService;
 
+    private IOrderStatusRepository orderStatusRepository;
+
     private PasswordEncoder passwordEncoder;
 
-    public SetupDataLoader(IUserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Autowired
-    public SetupDataLoader(IUserRepository userRepository, RoleServiceImpl roleService, IRoleRepository roleRepository, IUserRoleService userRoleService, PasswordEncoder passwordEncoder) {
+    public SetupDataLoader(IUserRepository userRepository, RoleServiceImpl roleService, IRoleRepository roleRepository, IUserRoleService userRoleService, IOrderStatusRepository orderStatusRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleService = roleService;
         this.roleRepository = roleRepository;
         this.userRoleService = userRoleService;
+        this.orderStatusRepository = orderStatusRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -55,6 +56,10 @@ public class SetupDataLoader implements
 
         createRoleIfNotFound("ROLE_USER");
         createRoleIfNotFound("ROLE_ADMIN");
+
+        createOrderStatusIfNotFound("IN_PROGRESS");
+        createOrderStatusIfNotFound("PENDING");
+        createOrderStatusIfNotFound("DELIVERED");
 
 
         Role adminRole = roleService.findOneAndEnsureExistByName("ROLE_ADMIN");
@@ -94,6 +99,17 @@ public class SetupDataLoader implements
             role.setName(name);
             roleRepository.save(role);
         }
+    }
+
+    @Transactional
+    void createOrderStatusIfNotFound(String name) {
+        Optional<OrderStatus> optionalOrderStatus = orderStatusRepository.findByName(name);
+        if(optionalOrderStatus.isEmpty()) {
+            OrderStatus orderStatus = new OrderStatus();
+            orderStatus.setName(name);
+            orderStatusRepository.save(orderStatus);
+        }
+
     }
 
 
