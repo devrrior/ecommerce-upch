9c5eb1fc94166ae781b33ec928707520441a8d7f
Fernando Guerrero <devrrior@gmail.com>
2022-11-03 21:06:20 -0600

    feat(controllers): change RequestMapping

--- src/main/java/com/school/ecommerceupch/controllers/UserController.java
+++ src/main/java/com/school/ecommerceupch/controllers/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("api/user")
 public class UserController {
     private final IUserService service;
 
