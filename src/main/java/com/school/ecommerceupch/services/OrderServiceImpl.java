60bbca3694ba27e7f592204cf9e76311010fe7ed
Fernando Guerrero <devrrior@gmail.com>
2022-11-04 20:32:51 -0600

    feat(services): add UniqueConstraintValidationException for OrderServiceImpl and UserServiceImpl

--- src/main/java/com/school/ecommerceupch/services/OrderServiceImpl.java
+++ src/main/java/com/school/ecommerceupch/services/OrderServiceImpl.java
@@ -12,6 +12,7 @@ import com.school.ecommerceupch.services.interfaces.IOrderService;
 import com.school.ecommerceupch.services.interfaces.IOrderStatusService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -73,7 +74,8 @@ public class OrderServiceImpl implements IOrderService {
 
         Order order = findOneAndEnsureExistById(id);
 
-        if (!order.getUser().getId().equals(userDetails.getUser().getId()))
+        if (userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))
+                || !order.getUser().getId().equals(userDetails.getUser().getId()))
             throw new AccessDeniedException();
 
         OrderStatus orderStatus = orderStatusService.findOneAndEnsureExistById(request.getOrderStatusId());
