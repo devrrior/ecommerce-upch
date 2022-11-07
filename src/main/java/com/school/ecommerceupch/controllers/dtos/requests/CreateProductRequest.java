0fb95f86eec4af5d41b7a5e507f79dc4475ff5c6
Fernando Guerrero <devrrior@gmail.com>
2022-11-06 22:34:36 -0600

    fix(dtos/request): remove NotNull annotation

--- src/main/java/com/school/ecommerceupch/controllers/dtos/requests/CreateProductRequest.java
+++ src/main/java/com/school/ecommerceupch/controllers/dtos/requests/CreateProductRequest.java
@@ -4,33 +4,30 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @Setter
 public class CreateProductRequest {
 
-    @NotNull
     @NotBlank
     private String title;
 
-    @NotNull
     @NotBlank
     private String description;
 
     private MultipartFile file;
 
-    @NotNull
     @NotBlank
     private Integer stock;
 
-    @NotNull
     @NotBlank
     private Float price;
 
-    @NotNull
     @NotBlank
     private Long userId;
 
--- src/main/java/com/school/ecommerceupch/controllers/dtos/requests/UpdateProductRequest.java
+++ src/main/java/com/school/ecommerceupch/controllers/dtos/requests/UpdateProductRequest.java
@@ -6,31 +6,27 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @Setter
 public class UpdateProductRequest {
 
-    @NotNull
     @NotBlank
     private String title;
 
-    @NotNull
     @NotBlank
     private String description;
 
     private MultipartFile file;
 
-    @NotNull
     @NotBlank
     private Integer stock;
 
-    @NotNull
     @NotBlank
     private Float price;
 
-    @NotNull
     @NotBlank
     private Long userId;
 
