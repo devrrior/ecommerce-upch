e5529a5c69cfcc52b93ad17d0607250c274d7d2e
Fernando Guerrero <devrrior@gmail.com>
2022-11-10 10:50:55 -0600

    feat(repository/IOrderRepository): add getOneByOrderStatus_Name method

--- src/main/java/com/school/ecommerceupch/repositories/IOrderRepository.java
+++ src/main/java/com/school/ecommerceupch/repositories/IOrderRepository.java
@@ -2,9 +2,13 @@ package com.school.ecommerceupch.repositories;
 
 import com.school.ecommerceupch.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface IOrderRepository extends JpaRepository<Order, Long> {
 
+    Optional<Order> getOneByOrderStatus_Name(String name);
 }
